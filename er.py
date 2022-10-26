0f7908da65bfef585b4b0db251c57a5726fef76a
haylodude <69397486+polarexe@users.noreply.github.com>
2022-10-26 09:32:23 +0200

    Add files via upload

---
+++ er.py
@@ -0,0 +1,4 @@
+arf = float(input('€ árfolyama: '))
+huf = int(input('ennyi forintod van: '))
+
+print(f'ez ennyit ér: {round(huf/arf, 2)}€')
---
+++ magyar.py
@@ -0,0 +1,5 @@
+a = int(input("téglalap 'a' oldala (cm): "))
+b = int(input("téglalap 'b' oldala (cm): "))
+
+print(f'téglalap kerülete: {2* (a + b)} cm')
+print(f'téglalap kerülete: {a * b} cm^2')
