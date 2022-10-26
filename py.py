25cffca532bf191e7e15661e7be5509110113c54
haylodude <69397486+polarexe@users.noreply.github.com>
2022-10-26 09:19:12 +0200

    Add files via upload

---
+++ py.py
@@ -0,0 +1,9 @@
+vnev_1 = input('első vezetéknév: ')
+vnev_2 = input('második vezetéknév: ')
+knev_1 = input('első keresztnév: ')
+knev_2 = input('második keresztnév: ')
+
+print(f'{vnev_1} {knev_1}')
+print(f'{vnev_1} {knev_2}')
+print(f'{vnev_2} {knev_1}')
+print(f'{vnev_2} {knev_2}')
---
+++ py891234.py
@@ -0,0 +1,2 @@
+havi = int(input('havi fizetés (Ft): '))
+print(f'éves fizetés: {12 * havi} Ft')
